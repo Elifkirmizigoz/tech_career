841e6489faa09a8c90c39850f909f1dcfd31279c
xthehonour <149792471+xthehonour@users.noreply.github.com>
2023-11-07 00:38:10 +0300

    Update C.c
    Anladığım tek kod

--- C DERSİ/C.c
+++ C DERSİ/C.c
@@ -4,7 +4,7 @@ int main (void){
      scanf("%f", &r);
      alan= pi* r*r;
      cevre= 2*pi*r;
-     printf(" /n/n %.2f yaricapli dairenin alani= ",r);
+     printf(" \n\n %.2f yaricapli dairenin alani= ",r);
      printf("%.2f ve cevresi= %.2f'dir.", alan,cevre);
      
 
