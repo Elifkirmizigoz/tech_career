0b2a9dd85105cbb161000006d5890d9d3458624c
Ekirmizigoz <elifkirmizigoz2003@gmail.com>
2023-11-06 16:02:30 +0300

    first commit

---
+++ C DERSİ/C.c
@@ -0,0 +1,15 @@
+int main (void){
+     float pi=22/7.0, r, cevre, alan;
+     printf("dairenin yaricapini giriniz: " );
+     scanf("%f", &r);
+     alan= pi* r*r;
+     cevre= 2*pi*r;
+     printf(" /n/n %.2f yaricapli dairenin alani= ",r);
+     printf("%.2f ve cevresi= %.2f'dir.", alan,cevre);
+     
+
+
+
+   return 0;  
+}
+
